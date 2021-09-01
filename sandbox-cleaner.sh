c8fe1a4099e2c6be9bc66f66da416b95643299f2
Don Schenck <don.schenck@gmail.com>
2021-09-01 09:25:01 -0400

    added builds, dc

--- sandbox-cleaner.sh
+++ sandbox-cleaner.sh
@@ -2,7 +2,10 @@ if [ $# -eq 0 ]; then
     echo "The app prefix must be supplied as a command-line parameter, e.g. ./sandbox-cleaner.sh myproject"
     exit 1
 fi
+
 project_prefix=$1
+
+oc delete $(oc get pods -o name | grep "$project_prefix")
 oc delete $(oc get secrets -o name | grep "$project_prefix")
 oc delete $(oc get routes -o name | grep "$project_prefix")
 oc delete $(oc get deploy -o name | grep "$project_prefix")
@@ -10,3 +13,6 @@ oc delete $(oc get imagestreams -o name | grep "$project_prefix")
 oc delete $(oc get services -o name | grep "$project_prefix")
 oc delete $(oc get configmaps -o name | grep "$project_prefix")
 oc delete $(oc get bc -o name | grep "$project_prefix")
+oc delete $(oc get builds -o name | grep "$project_prefix")
+oc delete $(oc get dc -o name | grep "$project_prefix")
+
