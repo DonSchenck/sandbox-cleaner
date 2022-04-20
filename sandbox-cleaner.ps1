80df980129d5a36fca8bfe25c6f9c9965281e9d7
Don Schenck <don.schenck@gmail.com>
2022-04-20 13:28:36 -0400

    Added requirements.txt

--- sandbox-cleaner.ps1
+++ sandbox-cleaner.ps1
@@ -1,4 +1,4 @@
-$PROJECT_NAME=oc project --short=true
+
 
 # if [ $# -eq 0 ]; then
 #    echo "This script will execute against your current project"PROJECT_NAME
@@ -7,19 +7,19 @@ $PROJECT_NAME=oc project --short=true
 #fi
 
 
-project_prefix=$(1:-$DEFAULT_VALUE)
-app_prefix="post"
+$app_prefix="post"
 
-oc delete $(oc get pods -o name | grep "$app_prefix")
-oc delete $(oc get secrets -o name | grep "$app_prefix")
-oc delete $(oc get routes -o name | grep "$app_prefix")
-oc delete $(oc get deploy -o name | grep "$app_prefix")
-oc delete $(oc get imagestreams -o name | grep "$app_prefix")
-oc delete $(oc get services -o name | grep "$app_prefix")
-oc delete $(oc get configmaps -o name | grep "$app_prefix")
-oc delete $(oc get bc -o name | grep "$app_prefix")
-oc delete $(oc get builds -o name | grep "$app_prefix")
-oc delete $(oc get dc -o name | grep "$app_prefix")
-oc delete $(oc get pvc -o name | grep "$app_prefix")
-oc delete $(oc get akc -o name | grep "$app_prefix")
+Invoke-Expression {oc get pods -o name | Select-String $app_prefix} | oc delete
+
+#oc delete $(oc get secrets -o name | grep "$app_prefix")
+#oc delete $(oc get routes -o name | grep "$app_prefix")
+#oc delete $(oc get deploy -o name | grep "$app_prefix")
+#oc delete $(oc get imagestreams -o name | grep "$app_prefix")
+#oc delete $(oc get services -o name | grep "$app_prefix")
+#oc delete $(oc get configmaps -o name | grep "$app_prefix")
+#oc delete $(oc get bc -o name | grep "$app_prefix")
+#oc delete $(oc get builds -o name | grep "$app_prefix")
+#oc delete $(oc get dc -o name | grep "$app_prefix")
+#oc delete $(oc get pvc -o name | grep "$app_prefix")
+#oc delete $(oc get akc -o name | grep "$app_prefix")
 
